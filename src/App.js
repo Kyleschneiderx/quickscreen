4712512530c2e764b0a8f67cfb1c30f5fd0fafa3
kyleschneiderx <kyleschneiderx@gmail.com>
2022-07-07 15:05:03 -0700

    added in gif

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { useState } from "react";
-import { Player } from 'video-react';
-import "../node_modules/video-react/dist/video-react.css";
 
 
-// 1)Heel toe walking x
-// 2)Single leg standing heel raise each leg x10x
-// 3)Stork standing foot position x
-// 4) Standing arms on wall
-// A-pelvic tiltsx
-// B-rotationx
-// C-side tilts x
-// 5) Standing hip rotation bent knee x
-// 6) Squat-narrow,  wide, rear load, single wleg pistol x
-// 7)Lunge both legs rotation right / left x
-// 8) Supine SLR 
-// 9) Diaphragm excursion 2-3 inches 
+
 
 
 function App() {
@@ -22,11 +9,12 @@ function App() {
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [answers, setAnswers] = useState([])
 
   const questions = [
     {
       text: "Can patient heel toe walk?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+      gif: '//gifs.com/embed/oZ0lnX',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -34,7 +22,7 @@ function App() {
     },
     {
       text: "Can patient single leg standing heel raise each leg 10 times?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+      gif: '//gifs.com/embed/mqnjkG',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -42,7 +30,7 @@ function App() {
     },
     {
       text: "Can patient do a stork standing foot position?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+      gif:'//gifs.com/embed/A62lrl',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -50,7 +38,7 @@ function App() {
     },
     {
       text: "Can patient do standing arms on wall pelvic tilt?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+      gif:'//gifs.com/embed/Og2DGr',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -58,7 +46,6 @@ function App() {
     },
     {
       text: "Can patient do standing arms on wall pelvic rotation?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -66,7 +53,6 @@ function App() {
     },
     {
       text: "Can patient do standing arms on wall pelvic side tilt?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -74,7 +60,6 @@ function App() {
     },
     {
       text: "Can patient do a standing hip rotation with bent knee?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -82,7 +67,6 @@ function App() {
     },
     {
       text: "Can patient do a squat-narrow,  wide, rear load, single leg pistol?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -90,7 +74,6 @@ function App() {
     },
     {
       text: "Can patient do a lunge both legs rotation right / left?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -98,7 +81,6 @@ function App() {
     },
     {
       text: "Can patient do a supine SLR",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -106,7 +88,6 @@ function App() {
     },
     {
       text: "Can patient do diaphragm excursion 2-3 inches?",
-      video:'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
       options: [
         { id: 0, text: "Yes", isCorrect: true },
         { id: 1, text: "No", isCorrect: false },
@@ -118,9 +99,14 @@ function App() {
 
   /* A possible answer was clicked */
   const optionClicked = (isCorrect) => {
+
+
     // Increment the score
     if (isCorrect) {
       setScore(score + 1);
+      setAnswers([...answers,{'Question': questions[currentQuestion].text,'Answer': "Yes"}])
+    }else{
+      setAnswers([...answers, {'Question': questions[currentQuestion].text,'Answer': "No"}])
     }
 
     if (currentQuestion + 1 < questions.length) {
@@ -135,9 +121,11 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setShowResults(false);
+    setAnswers([])
   };
 
   return (
+    
     <div className="container">
       {/* 1. Header  */}
       <div className="flex flex-col items-center h-screen justify-center mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
@@ -152,23 +140,36 @@ function App() {
         <div className="final-results">
           <h1>Final Results</h1>
           <h2>
-            {score} out of {questions.length} correct - (
-            {(score / questions.length) * 100}%)
+            {questions.length - score} Pelvic Floor Concerns
           </h2>
-          <button onClick={() => restartGame()}>Restart game</button>
+
+          <div>
+            {answers.map((item)=>{
+              return(
+                <div>
+                  {item.Question}:
+                  {item.Answer}
+                </div>
+              )
+            })}
+          </div>
+
+          <button onClick={() => restartGame()}>Restart exam</button>
         </div>
       ) : (
         /* 5. Question Card  */
-        <div className="rounded bg-gray-100">
+        <div className="rounded bg-gray-100 w-96">
           {/* Current Question  */}
           <h2>
             Question: {currentQuestion + 1} out of {questions.length}
           </h2>
           <h2>Example Video:</h2>
-          <Player
+          <iframe src={questions[currentQuestion].gif} className="w-full"></iframe>
+
+          {/* <Player
             playsInline
             src={questions[currentQuestion].video}
-          />
+          /> */}
 
           <h3 className="question-text">{questions[currentQuestion].text}</h3>
 
